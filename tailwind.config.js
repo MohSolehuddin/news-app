a5eeae5f23a1dcf2d66bfa5611039d441c0cb700
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2024-12-31 11:19:31 +0700

    Setup nativewind for styling

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./app/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: "#133E87",
+        customWarning: "#FFE31A",
+        customSuccess: "#4CAF50",
+        customError: "#F44336",
+        customInfo: "#2196F3",
+        customBlack: "#1A1A19",
+        bluishWhite: "#F2F4FF",
+        softBeige: "#EFE9E7",
+        deepDarkBlue: "#111344",
+        darkPurple: "#2F195F",
+        blueBlack: "#0F1020",
+      },
+    },
+  },
+  plugins: [],
+};
