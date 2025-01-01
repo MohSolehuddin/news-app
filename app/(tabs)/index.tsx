d26b938a072dc868e36a85d25fd1c7e552409704
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-01-01 16:28:49 +0700

    FEAT: search, get new news in country, get all source, filtering source in app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,13 +3,19 @@ import NewsCard from "@/components/NewsCard";
 import NewsContainer from "@/components/NewsContainer";
 import SafeAreaShell from "@/components/SafeAreaShell";
 import SearchInput from "@/components/SearchInput";
-import { fetchNewsByCategory } from "@/redux/features/fecthingByCategoriesSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/features/hooks";
+import {
+  fetchAllSources,
+  lastNewsInCountry,
+  searchNews,
+} from "@/redux/features/newsSlice";
 import { useEffect, useState } from "react";
 import { ScrollView, Text, View } from "react-native";
 const index = () => {
   const dispatch = useAppDispatch();
-  const { newsByCategory } = useAppSelector((state) => state.newsByCategory);
+  const { newsByCategory, sources } = useAppSelector(
+    (state) => state.newsByCategory
+  );
   const [activeCategory, setActiveCategory] = useState("");
   const categories = [
     "business",
@@ -25,14 +31,22 @@ const index = () => {
     setActiveCategory(category);
   };
   useEffect(() => {
-    if (activeCategory) {
-      dispatch(fetchNewsByCategory(activeCategory));
-    }
-  }, [activeCategory]);
+    dispatch(fetchAllSources());
+    dispatch(lastNewsInCountry());
+  }, []);
 
-  useEffect(() => {
-    console.log(newsByCategory);
-  }, [newsByCategory]);
+  const handleSearch = (text: string) => {
+    dispatch(searchNews(text));
+  };
+  // useEffect(() => {
+  //   if (activeCategory) {
+  //     dispatch(fetchNewsByCategory(activeCategory));
+  //   }
+  // }, [activeCategory]);
+
+  // useEffect(() => {
+  //   console.log(newsByCategory);
+  // }, [newsByCategory]);
   return (
     <SafeAreaShell isScrollView={false}>
       <View className="fixed top-0 left-0 z-50 h-36 w-full">
@@ -41,15 +55,24 @@ const index = () => {
           activeCategory={activeCategory}
           handleCategoryClick={handleCategoryClick}
         />
-        <SearchInput />
+        <SearchInput onSubmit={handleSearch} />
       </View>
       <ScrollView>
         <NewsContainer title="Latest News">
-          {newsByCategory.map((item, index) => (
-            <NewsCard key={index}>
-              <Text>{item.title}</Text>
-            </NewsCard>
-          ))}
+          {!activeCategory &&
+            newsByCategory.map((item, index) => (
+              <NewsCard key={index}>
+                <Text>{item.title}</Text>
+              </NewsCard>
+            ))}
+          {activeCategory &&
+            sources
+              .filter((item) => item.category === activeCategory)
+              .map((item, index) => (
+                <NewsCard key={index}>
+                  <Text>{item.name}</Text>
+                </NewsCard>
+              ))}
         </NewsContainer>
       </ScrollView>
     </SafeAreaShell>
