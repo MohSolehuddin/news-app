2e3d5d00f9aaee9666ca95759bab5ddd28e6f4cf
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-01-02 20:59:39 +0700

    delete advanced filter

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,17 +1,10 @@
-import { FontAwesome } from "@expo/vector-icons";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import { useState } from "react";
 import { TextInput, View } from "react-native";
-import ModalContainer from "./ModalContainer";
-import { Text } from "react-native";
-import CustomButton from "./CustomButton";
 
 interface SearchInputProps {
   onSubmit: (text: string, ...args: any) => void;
 }
 const SearchInput = ({ onSubmit, ...props }: SearchInputProps) => {
-  const [modalVisible, setModalVisible] = useState(false);
-
   return (
     <View>
       <TextInput
@@ -25,23 +18,6 @@ const SearchInput = ({ onSubmit, ...props }: SearchInputProps) => {
         size={24}
         color="#9CA3AF"
         className="absolute top-4 left-6"
-      />
-      <FontAwesome
-        name="filter"
-        size={24}
-        color="#9CA3AF"
-        className="absolute top-4 right-6"
-        onPress={() => setModalVisible(true)}
-      />
-      <ModalContainer
-        isOpen={modalVisible}
-        setIsOpen={setModalVisible}
-        children={
-          <View className="flex gap-6">
-            <Text>Filter</Text>
-            <CustomButton onPress={() => setModalVisible(false)} text="Apply" />
-          </View>
-        }
       />
     </View>
   );
